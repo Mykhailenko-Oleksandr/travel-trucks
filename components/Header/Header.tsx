0fb4c8212c3fa6f6bc0f09214f55520d76adc6f0
Sapper <saniamykhailenko@gmail.com>
2026-01-05 17:56:58 +0200

    add logo and start header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,24 @@
+import Link from "next/link";
 import css from "./Header.module.css";
 
 export default async function Header() {
   return (
     <header className={css.header}>
-      <div className="container">
-        <h2>Header</h2>
+      <div className={`container ${css.headerContainer}`}>
+        <Link className={css.logo} href="/" aria-label="logotype">
+          <svg className={css.logoIcon} width={136} height={16}>
+            <use href="/sprite.svg#icon-logo" />
+          </svg>
+        </Link>
+
+        <nav className={css.navigationBox}>
+          <Link className={css.navigationLink} href="/">
+            Home
+          </Link>
+          <Link className={css.navigationLink} href="/">
+            Catalog
+          </Link>
+        </nav>
       </div>
     </header>
   );
