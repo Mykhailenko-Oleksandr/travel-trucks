123dfbceabf776dc15a16d46ec3378b27522f40d
Sapper <saniamykhailenko@gmail.com>
2026-01-06 13:05:13 +0200

    add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+import Link from "next/link";
 import css from "./Home.module.css";
 
 export default function Home() {
   return (
-    <section>
+    <section className={css.hero}>
       <div className="container">
-        <h1>Home Page</h1>
+        <h1 className={css.heroTitle}>Campers of your dreams</h1>
+        <p className={css.heroText}>
+          You can find everything you want in our catalog
+        </p>
+        <Link className={css.heroLink} href="/catalog">
+          View Now
+        </Link>
       </div>
     </section>
   );
