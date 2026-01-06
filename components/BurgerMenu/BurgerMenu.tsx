03861d0f1b056d3f9a4d59ab8e133cc1a37ab327
Sapper <saniamykhailenko@gmail.com>
2026-01-06 12:09:48 +0200

    add close burgerMenu

--- components/BurgerMenu/BurgerMenu.tsx
+++ components/BurgerMenu/BurgerMenu.tsx
@@ -2,34 +2,80 @@
 
 import Link from "next/link";
 import css from "./BurgerMenu.module.css";
+import { usePathname } from "next/navigation";
+import { MouseEvent, useEffect, useRef } from "react";
 
 interface BurgerMenuProps {
   closeMenu: () => void;
+  isOpen: boolean;
 }
 
-export default function BurgerMenu({ closeMenu }: BurgerMenuProps) {
+export default function BurgerMenu({ closeMenu, isOpen }: BurgerMenuProps) {
+  const pathname = usePathname();
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  function handleClickBackdrop(e: MouseEvent<HTMLDivElement>) {
+    if (e.target === menuRef.current) {
+      closeMenu();
+    }
+  }
+
+  useEffect(() => {
+    if (!isOpen) return;
+
+    function onEsc(e: KeyboardEvent) {
+      if (e.key === "Escape") closeMenu();
+    }
+
+    document.body.classList.toggle("no-scroll", isOpen);
+    window.addEventListener("keydown", onEsc);
+
+    return () => {
+      document.body.classList.remove("no-scroll");
+      window.removeEventListener("keydown", onEsc);
+    };
+  }, [isOpen, closeMenu]);
+
   return (
-    <div className={css.modalNavbar}>
+    <div
+      ref={menuRef}
+      className={`${css.backdrop} ${isOpen ? css.modalIsOpen : ""}`}
+      onClick={handleClickBackdrop}>
       <div className={`container ${css.mobileMenu} `}>
         <div className={css.mobileMenuTop}>
-          <Link className={css.logo} href="/" aria-label="logotype">
-            <svg className={css.logoIcon} width={136} height={16}>
+          <Link
+            className={css.logo}
+            href="/"
+            aria-label="logotype">
+            <svg
+              className={css.logoIcon}
+              width={136}
+              height={16}>
               <use href="/sprite.svg#icon-logo" />
             </svg>
           </Link>
 
-          <button className={css.menuCloseBtn} onClick={closeMenu}>
-            <svg className={css.menuIcon} width="24" height="24">
+          <button
+            className={css.menuCloseBtn}
+            onClick={closeMenu}>
+            <svg
+              className={css.menuIcon}
+              width="24"
+              height="24">
               <use href="/sprite.svg#icon-close" />
             </svg>
           </button>
         </div>
 
         <nav className={css.navigationBox}>
-          <Link className={css.navigationLink} href="/">
+          <Link
+            className={`${css.navigationLink} ${pathname === "/" ? css.accent : ""}`}
+            href="/">
             Home
           </Link>
-          <Link className={css.navigationLink} href="/">
+          <Link
+            className={`${css.navigationLink} ${pathname === "/catalog" ? css.accent : ""}`}
+            href="/catalog">
             Catalog
           </Link>
         </nav>
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,32 +4,52 @@ import Link from "next/link";
 import css from "./Header.module.css";
 import { useState } from "react";
 import BurgerMenu from "../BurgerMenu/BurgerMenu";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
   const [isMenuOpen, setMenuOpen] = useState(false);
 
   return (
     <header className={css.header}>
       <div className={`container ${css.headerContainer}`}>
-        <Link className={css.logo} href="/" aria-label="logotype">
-          <svg className={css.logoIcon} width={136} height={16}>
+        <Link
+          className={css.logo}
+          href="/"
+          aria-label="logotype">
+          <svg
+            className={css.logoIcon}
+            width={136}
+            height={16}>
             <use href="/sprite.svg#icon-logo" />
           </svg>
         </Link>
 
-        <button className={css.menuBtn} onClick={() => setMenuOpen(true)}>
-          <svg className={css.menuIcon} width="24" height="24">
+        <button
+          className={css.menuBtn}
+          onClick={() => setMenuOpen(true)}>
+          <svg
+            className={css.menuIcon}
+            width="24"
+            height="24">
             <use href="/sprite.svg#icon-menu" />
           </svg>
         </button>
 
-        {isMenuOpen && <BurgerMenu closeMenu={() => setMenuOpen(false)} />}
+        <BurgerMenu
+          closeMenu={() => setMenuOpen(false)}
+          isOpen={isMenuOpen}
+        />
 
         <nav className={css.navigationBox}>
-          <Link className={css.navigationLink} href="/">
+          <Link
+            className={`${css.navigationLink} ${pathname === "/" ? css.accent : ""}`}
+            href="/">
             Home
           </Link>
-          <Link className={css.navigationLink} href="/">
+          <Link
+            className={`${css.navigationLink} ${pathname === "/catalog" ? css.accent : ""}`}
+            href="/catalog">
             Catalog
           </Link>
         </nav>
